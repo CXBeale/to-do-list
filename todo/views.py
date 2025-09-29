a8c26576e94ec809d8b8213ab0bf7eb1285a066e
Christian <cxbeale@gmail.com>
2025-09-29 15:00:13 +0100

    add html hpome template

--- todo/views.py
+++ todo/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Welcome to the To-Do app!")
+    return render(request, 'todo/home.html')
 
