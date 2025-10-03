32841643d3344a3905bbaa5ffdf104cf4b88e82d
Christian <cxbeale@gmail.com>
2025-10-03 12:52:13 +0100

    Style and center error messages on login and signup pages; improve form UX

--- todolist/settings.py
+++ todolist/settings.py
@@ -151,8 +151,11 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Extra settings for Whitenoise
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Email backend for testing (prints emails to console/logs)
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
