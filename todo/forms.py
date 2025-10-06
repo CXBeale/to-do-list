c953a897cbcafb64978472d7c28ffe02c14c5afa
Christian <cxbeale@gmail.com>
2025-10-06 16:41:44 +0100

    Add list option to Task model and TaskForm

--- todo/forms.py
+++ todo/forms.py
@@ -1,7 +1,7 @@
 from django import forms
-from .models import Task
+from .models import Task, List
 
 class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
-        fields = ['title', 'due_date']
+        fields = ['title', 'due_date', 'list']  # exclude 'completed'
