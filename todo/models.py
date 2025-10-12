64fd49e26b04772d122763be49753e6edcd536da
Christian <cxbeale@gmail.com>
2025-10-12 10:27:31 +0100

    remove darte from task model

--- todo/models.py
+++ todo/models.py
@@ -6,7 +6,7 @@ class Task(models.Model):
     title = models.CharField(max_length=255)  # The name or description of the task
     completed = models.BooleanField(default=False)  # True if the task is done
     created_at = models.DateTimeField(auto_now_add=True)  # When the task was created
-    due_date = models.DateField(null=True, blank=True)  # Optional due date
+    #due_date = models.DateField(null=True, blank=True)  # Optional due date
     user = models.ForeignKey(User, on_delete=models.CASCADE)  # Link task to a user
     list = models.ForeignKey('List', on_delete=models.CASCADE, related_name='tasks', null=True, blank=True)  # Link task to a list (optional)
     
